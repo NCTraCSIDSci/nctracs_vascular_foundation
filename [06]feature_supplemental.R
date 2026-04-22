33f3c679c3ad19245ece6c7124bc7109f64bf1b9
tmcintee <56977435+tmcintee@users.noreply.github.com>
2026-04-22 12:18:22 -0400

    (m) Add metadata and licensing to feature supplemental
    Added metadata and licensing information to the feature supplemental file.

--- [06]feature_supplemental.R
+++ [06]feature_supplemental.R
@@ -1,4 +1,11 @@
 # Feature supplemental
+# First feature rollup stage.
+# Author(s): Tomas McIntee
+# Last updated: 2025-04-17
+#
+# © 2024, The University of North Carolina at Chapel Hill. The code is licensed 
+# under the MIT License and permission is granted to use in accordance with the 
+# MIT License.
 # This workbook will seek to collect and synthesize supplemental data, including:
 #
 # * Creation of index and decision dates
